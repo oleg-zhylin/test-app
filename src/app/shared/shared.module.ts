386336ee70514f70bf80ff137a3d9075d7eef59b
Oleg Zhilin <o.zhilin@yahoo.com>
2018-05-10 20:53:28 +0100

    Fix error in shared module (run and check console)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from "./form.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   declarations: [FormComponent],
-  exports: [FormComponent]
+  exports: [FormComponent, ReactiveFormsModule]
 })
 export class SharedModule { }
+
